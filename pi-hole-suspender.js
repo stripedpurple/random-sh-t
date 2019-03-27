e86d903afe1a823c1b43caa15630fcfdcdeca516
Austin Barrett <virus.cmd@gmail.com>
2019-03-26 23:36:37 -0400

    wip

--- pi-hole-suspender.js
+++ pi-hole-suspender.js
@@ -1,8 +1,9 @@
 window.onload = function () {
+    document.body.style.backgroundColor = 'red';
     var modal = document.createElement('div');
 
     modal.innerHTML = '<div style="background: #fefefe; padding: 2rem 1.5rem; margin: 45% auto auto auto;"> <form action="#" id="pihole-form"> <div style="display: inline-block"> <label for="hour">Hours</label><br> <input type="number" name="hour"> </div> <div style="display: inline-block"> <label for="min">Minutes</label><br> <input type="number" name="min"> </div> <div style="display: inline-block"> <label for="sec">Seconds</label><br> <input type="number" name="sec"> </div> <div> <input type="button" value="suspend" id="pihole-submit"> </div> </form> </div>';
-    document.body.appendChild(modal)
+    document.body.appendChild(modal);
 
     document.getElementById('pihole-submit').addEventListener('click', function(e){
         var apikey = document.getElementById('pihole').dataset.api;
@@ -20,16 +21,17 @@ window.onload = function () {
     document.getElementById('pihole-form').addEventListener('submit', function(e){
         e.preventDefault();
     });
-}
 
-var disable = function(time, apikey, cb) {
-    fetch(`http://pi.hole/admin/api.php?disable=${time}&auth=${apikey}`)
-        .then(res => {
-            return res;
-        }).catch(err => console.error(err));
-    cb();
-}
-
-var cleanup = function(){
-    window.close();
+    var disable = function(time, apikey, cb) {
+        fetch(`http://pi.hole/admin/api.php?disable=${time}&auth=${apikey}`)
+            .then(res => {
+                return res;
+            }).catch(err => console.error(err));
+        cb();
+    }
+    
+    var cleanup = function(){
+        window.close();
+    }
+    
 }
